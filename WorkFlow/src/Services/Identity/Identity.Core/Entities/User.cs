3cf5f34fce6adc0b0822590e136430b30d3e8f2e
ZaharKaras <zahar.karas@gmail.com>
2024-01-11 23:25:15 +0300

    add entities

--- WorkFlow/src/Services/Identity/Identity.Core/Entities/User.cs
+++ WorkFlow/src/Services/Identity/Identity.Core/Entities/User.cs
@@ -1,14 +1,6 @@
 ﻿using AspNetCore.Identity.MongoDbCore.Models;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDbGenericRepository.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Identity.Core.Entities
 {
