5de0ea3802d8ed8be0ca87ab7f6e1d7d7cf19290
ZaharKaras <zahar.karas@gmail.com>
2024-02-02 03:28:26 +0300

    add infrastructure

---
+++ WorkFlow/Board.Infastructure/Configuration/BoardConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Board.Infrastructure.Configuration
+{
+	public class BoardConfiguration : IEntityTypeConfiguration<Core.Entities.Board>
+	{
+		public void Configure(EntityTypeBuilder<Core.Entities.Board> builder)
+		{
+			builder.ToTable("Boards");
+
+			builder.HasKey(e => e.Id);
+
+			builder.Property(e => e.Name)
+				.IsRequired()
+				.HasMaxLength(255);
+
+			builder.HasMany(b => b.BoardUsers)
+				.WithOne()
+				.HasForeignKey(bu => bu.BoardId)
+				.OnDelete(DeleteBehavior.Cascade); 
+		}
+	}
+	
+}
