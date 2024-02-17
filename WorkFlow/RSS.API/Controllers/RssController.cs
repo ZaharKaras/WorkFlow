957bca42b3124a1de63657aa558d901ff4ac49de
ZaharKaras <zahar.karas@gmail.com>
2024-02-17 14:38:18 +0300

    add main library

---
+++ WorkFlow/RSS.API/Controllers/RssController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace RSS.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RssController : ControllerBase
+	{
+
+		[HttpPost]
+		[Route("feeds")]
+		public async Task<ActionResult<SyndicationItem>> GetFeedAsync(
+			[FromBody] string url, CancellationToken token)
+		{
+			using var reader = XmlReader.Create(url);
+			var feed = SyndicationFeed.Load(reader);
+
+			var post = feed.Items.FirstOrDefault();
+
+			return Ok(post);
+		}
+
+	}
+}
