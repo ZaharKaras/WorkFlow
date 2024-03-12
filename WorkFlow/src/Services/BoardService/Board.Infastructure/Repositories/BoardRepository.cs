18d55de208287a1776a5da597294b74227347b46
ZaharKaras <zahar.karas@gmail.com>
2024-03-12 14:21:09 +0300

    add explicit naming

--- WorkFlow/src/Services/BoardService/Board.Infastructure/Repositories/BoardRepository.cs
+++ WorkFlow/src/Services/BoardService/Board.Infastructure/Repositories/BoardRepository.cs
@@ -11,8 +11,8 @@ namespace Board.Infrastructure.Repositories
 		public Task<IEnumerable<Core.Entities.Board>> GetByUserIdAsync(Guid userId, CancellationToken token = default)
 		{
 			var boardIds = _context.BoardsUsers
-				.Where(bu => bu.UserId == userId)
-				.Select(bu => bu.BoardId)
+				.Where(boardUser => boardUser.UserId == userId)
+				.Select(boardUser => boardUser.BoardId)
 				.ToList();
 
 			var boards = _context.Boards
