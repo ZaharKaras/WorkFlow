65ce59c4585ba0dfcd3b3b05d1c8073a0ad0b98f
ZaharKaras <zahar.karas@gmail.com>
2024-03-12 14:18:34 +0300

    move from minimap api to controllers

---
+++ WorkFlow/RSS.API/Controllers/RssController.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RSS.Application.DTOs;
+using RSS.Application.Feeds.Create;
+using RSS.Application.Feeds.Delete;
+using RSS.Application.Feeds.Get;
+using RSS.Application.Feeds.List;
+
+namespace RSS.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RssController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public RssController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		[Route("feeds")]
+		public async Task<IActionResult> AddFeedAsync(
+			[FromBody] CreateFeedCommand command, CancellationToken token)
+		{
+			await _mediator.Send(command, token);
+
+			return NoContent();
+		}
+
+		[HttpGet]
+		[Route("users/{userId}")]
+		public async Task<ActionResult<IEnumerable<FeedDTO>>> GetFeedsAsync(
+			[FromRoute] Guid userId, CancellationToken token)
+		{
+			var query = new ListFeedQuery(userId);
+			var result = await _mediator.Send(query, token);
+
+			return Ok(result);
+		}
+
+		[HttpGet]
+		[Route("feeds/{feedId}")]
+		public async Task<ActionResult<IEnumerable<FeedDTO>>> GetFeedItemsAsync(
+			[FromRoute] Guid feedId, CancellationToken token)
+		{
+			var query = new GetFeedQuery(feedId);
+			var result = await _mediator.Send(query, token);
+
+			return Ok(result);
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteFeedAsync(
+			[FromBody] Guid id, CancellationToken token)
+		{
+			var command = new DeleteFeedCommand(id);
+			await _mediator.Send(command, token);
+
+			return NoContent();
+		}
+	}
+}
